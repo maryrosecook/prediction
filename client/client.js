af5e338d03a39ce42a40a521cd634febf4260a0e
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-07-19 16:25:32 -0400

    Dumb lockstep server.
    * Keypresses sent to server.
    * Server simulates.
    * Server sends player state to client.
    * Client draws.

---
+++ client/client.js
@@ -0,0 +1,67 @@
+var startSendingKeypresses = function(socket) {
+  var sendKeyPress = function(key, down) {
+    socket.emit('keypress', { key:key, down:down });
+  };
+
+	window.addEventListener('keydown', function(e) {
+    sendKeyPress(e.keyCode, true);
+  });
+
+	window.addEventListener('keyup', function(e) {
+    sendKeyPress(e.keyCode, false);
+  });
+};
+
+var StateListener = function(socket) {
+  var data = {};
+  socket.on('update', function(message) {
+    data[message.id] = message.data;
+  });
+
+  socket.on('death', function(message) {
+    delete data[message.id];
+  });
+
+  this.getData = function() {
+    return data;
+  };
+};
+
+var drawPlayer = function(ctx, player) {
+  var ship = {
+    w: 20,
+    h: 20,
+  };
+
+  ctx.save();
+  ctx.translate(player.position.x + ship.w / 2,
+                player.position.y + ship.h / 2);
+  ctx.rotate(player.angle); // radians
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(-(ship.w / 2), -(ship.h / 2), ship.w, ship.h);
+  ctx.fillStyle = "#f00";
+  ctx.fillRect(-(ship.w / 2), -(ship.h / 2), ship.w, ship.h / 10);
+
+  ctx.restore();
+};
+
+window.onload = function() {
+  var socket = io.connect('http://localhost:5000');
+  var stateListener = new StateListener(socket);
+  startSendingKeypresses(socket);
+
+  var ctx = document.getElementById("canvas").getContext('2d');
+
+  var render = function() {
+    var data = stateListener.getData();
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (var i in data) {
+      drawPlayer(ctx, data[i]);
+    };
+
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
---
+++ server/server.js
@@ -0,0 +1,91 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app, { log: false }),
+    fs = require('fs');
+
+app.listen(5000);
+
+function handler (req, res) {
+  var file = req.url === "/" ? "/client/index.html" : req.url;
+  fs.readFile(__dirname + "/.." + file, function (err, data) {
+    if (err) {
+      console.log(err)
+      res.writeHead(404);
+      res.end();
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+}
+
+var players = {};
+
+var Player = function(id) {
+  this.id = id;
+  this.angle = 0;
+  this.position = { x: 50, y:50 };
+  this.vector = { x:0, y:0 };
+  this.state = [];
+};
+
+var ROTATION_FACTOR = 0.005;
+var THRUST_FACTOR = 0.0001;
+
+Player.prototype = {
+  update: function() {
+    if (this.state[37]) { // left
+      this.angle -= ROTATION_FACTOR;
+    } else if (this.state[39]) { // right
+      this.angle += ROTATION_FACTOR;
+    } else if (this.state[38]) { // up
+      this.vector.x += Math.sin(this.angle) * THRUST_FACTOR;
+      this.vector.y += -Math.cos(this.angle) * THRUST_FACTOR;
+    }
+
+    this.position.x += this.vector.x;
+    this.position.y += this.vector.y;
+  },
+
+  change: function(data) {
+    this.state[data.key] = data.down;
+  },
+
+  toData: function() {
+    return {
+      id: this.id,
+      data: {
+        angle: this.angle,
+        position: this.position
+      }
+    };
+  }
+};
+
+io.sockets.on('connection', function (socket) {
+  players[socket.id] = new Player(socket.id);
+  io.sockets.emit('update', players[socket.id].toData());
+
+  socket.on('keypress', function(data) {
+    players[socket.id].change(data);
+  });
+
+  socket.on('disconnect', function() {
+    delete players[socket.id];
+    io.sockets.emit('death', { id: socket.id });
+  });
+});
+
+// send updates to clients
+setInterval(function() {
+  for (var i in players) {
+    io.sockets.emit('update', players[i].toData());
+  }
+}, 0);
+
+
+// game tick
+setInterval(function() {
+  for (var i in players) {
+    players[i].update();
+  }
+}, 0);
